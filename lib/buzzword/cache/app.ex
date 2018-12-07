b691fefe4c0d87737ddef901aa05a1b6edf6c93c
RaymondLoranger <Raymond.Loranger@bell.net>
2018-12-06 23:26:23 -0500

    0.1.0

--- lib/buzzword/cache/app.ex
+++ lib/buzzword/cache/app.ex
@@ -9,16 +9,12 @@ defmodule Buzzword.Cache.App do
 
   @info_path Application.get_env(:logger, :info_log)[:path]
   @warn_path Application.get_env(:logger, :warn_log)[:path]
-  @timeout_in_ms 100
 
   @spec start(Application.start_type(), term) :: {:ok, pid}
   def start(_type, :ok) do
     unless Mix.env() == :test do
       [@info_path, @warn_path] |> Enum.each(&Reset.clear_log/1)
     end
-
-    # Wait in case parent apps are clearing log files...
-    Process.sleep(@timeout_in_ms)
 
     [
       # Child spec relying on use GenServer...
