078a3081c4a2b2133f48fa213c9319e9cc293f66
RaymondLoranger <Raymond.Loranger@bell.net>
2018-12-07 09:03:47 -0500

    0.1.0

--- lib/buzzword/cache/app.ex
+++ lib/buzzword/cache/app.ex
@@ -5,17 +5,9 @@ defmodule Buzzword.Cache.App do
 
   alias __MODULE__
   alias Buzzword.Cache.Server
-  alias Log.Reset
-
-  @info_path Application.get_env(:logger, :info_log)[:path]
-  @warn_path Application.get_env(:logger, :warn_log)[:path]
 
   @spec start(Application.start_type(), term) :: {:ok, pid}
   def start(_type, :ok) do
-    unless Mix.env() == :test do
-      [@info_path, @warn_path] |> Enum.each(&Reset.clear_log/1)
-    end
-
     [
       # Child spec relying on use GenServer...
       {Server, :ok}
