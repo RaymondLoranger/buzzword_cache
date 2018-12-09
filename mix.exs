f6dcccac2d2ae27275028334b08a8018f80993f9
RaymondLoranger <Raymond.Loranger@bell.net>
2018-12-08 21:06:12 -0500

    0.1.0

--- mix.exs
+++ mix.exs
@@ -25,7 +25,7 @@ defmodule Buzzword.Cache.MixProject do
     [
       {:mix_tasks,
        github: "RaymondLoranger/mix_tasks", only: :dev, runtime: false},
-      {:log_reset, github: "RaymondLoranger/log_reset"},
+      {:log_reset, "~> 0.1"},
       {:persist_config, "~> 0.1"},
       {:logger_file_backend, "~> 0.0.9"},
       {:earmark, "~> 1.0", only: :dev},
