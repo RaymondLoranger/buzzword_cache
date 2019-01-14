9564bc3524b34f84e55faecc4d89346f9f1f4cc4
RaymondLoranger <Raymond.Loranger@bell.net>
2019-01-14 14:52:59 -0500

    0.1.3

--- lib/buzzword/cache/log.ex
+++ lib/buzzword/cache/log.ex
@@ -1,6 +1,5 @@
 defmodule Buzzword.Cache.Log do
   use File.Only.Logger
-  use PersistConfig
 
   warn :line_incorrect, {path, line, index} do
     """
@@ -12,9 +11,7 @@ defmodule Buzzword.Cache.Log do
       #{inspect(line, pretty: true)}
     • Correct format:
       "<phrase of 3 or more chars>,<integer points greater than 0>"
-    • App: #{Mix.Project.config()[:app]}
-    • Library: #{@app}
-    • Module: #{inspect(__MODULE__)}
+    #{from()}
     """
   end
 end
--- mix.exs
+++ mix.exs
@@ -4,7 +4,7 @@ defmodule Buzzword.Cache.MixProject do
   def project do
     [
       app: :buzzword_cache,
-      version: "0.1.2",
+      version: "0.1.3",
       elixir: "~> 1.7",
       start_permanent: Mix.env() == :prod,
       deps: deps(),
