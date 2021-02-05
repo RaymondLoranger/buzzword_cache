85a47452658b8164aca43b24ac3bff0f7d33b965
RaymondLoranger <Raymond.Loranger@bell.net>
2021-02-04 19:50:42 -0500

    0.1.17

--- mix.exs
+++ mix.exs
@@ -4,7 +4,7 @@ defmodule Buzzword.Cache.MixProject do
   def project do
     [
       app: :buzzword_cache,
-      version: "0.1.16",
+      version: "0.1.17",
       elixir: "~> 1.11",
       start_permanent: Mix.env() == :prod,
       deps: deps()
